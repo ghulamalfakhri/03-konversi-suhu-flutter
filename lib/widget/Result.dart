1d4f92153dca4091adadc31dd35788d985a0a394
ghulam <ghulamalfakhri@gmail.com>
2022-03-28 15:26:07 +0700

    03-konversi-suhu-flutter

---
+++ lib/widget/Result.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+//Jihan Rahadatul Aisy (2031710034)
+
+class Result extends StatelessWidget {
+  const Result({
+    Key? key,
+    required this.result,
+    required this.title,
+  }) : super(key: key);
+
+  final double result;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '$title',
+          style: TextStyle(fontSize: 15, color: Colors.black),
+        ),
+        Text(
+          '$result',
+          style: TextStyle(fontSize: 25, color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
